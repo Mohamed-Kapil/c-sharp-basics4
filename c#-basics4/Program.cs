fd2136511aac240c818e6d0d14a9af88fe4ef2bc
kapil <mohamedkapil31@gmail.com>
2026-09-03 07:40:46 +0300

    1 question

--- c#-basics4/Program.cs
+++ c#-basics4/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-           
+            #region 1 question
+
+            double[] prices = { 25.5, 40.0, 33.75 };
+
+            Console.WriteLine(prices[1]);
+
+            #endregion
+
+            #region 2 question
+            #endregion
+
+            #region 3 question
+            #endregion
+
+            #region 4 question
+            #endregion
+
+            #region 5 question
+            #endregion
+
+            #region 6 question
+            #endregion
+
+            #region 7 question
+            #endregion
+
+            #region 8 question
+            #endregion
+
+            #region 9 question
+            #endregion
+
+            #region 10 question
+            #endregion
+
+            #region 11 question
+            #endregion
         }
     }
 }
